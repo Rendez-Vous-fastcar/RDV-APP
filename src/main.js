b088cbffe7be26d0c74344f8b88592728911bf97
Rendez-Vous-fastcar <coppamilanosanremo1906@gmail.com>
2025-07-09 13:37:01 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,14 +4,15 @@ const vapidKey = "SXJqoSHFRzWYTEWhDzAKdmDdtO8VVKRNVkFArY7q77c";
 
 Notification.requestPermission().then(permission => {
   if (permission === "granted") {
-    getToken(messaging, { vapidKey }).then(token => {
-      console.log("FCM Token:", token);
-      fetch("/.netlify/functions/saveToken", {
-        method: "POST",
-        body: JSON.stringify({ token }),
-        headers: { "Content-Type": "application/json" },
+    getToken(messaging, { vapidKey: "SXJqoSHFRzWYTEWhDzAKdmDdtO8VVKRNVkFArY7q77c", serviceWorkerRegistration: registration })
+      .then((currentToken) => {
+        if (currentToken) {
+          fetch('/.netlify/functions/saveToken', {
+            method: 'POST',
+            body: JSON.stringify({ token: currentToken }),
+          });
+        }
       });
-    });
   }
 });
 
