c9716a1e37dbab9474c40ad0346efb4a21506a69
Rendez-Vous-fastcar <coppamilanosanremo1906@gmail.com>
2025-07-09 12:57:06 +0200

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,11 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "TUA_API_KEY",
-  authDomain: "tuo-progetto.firebaseapp.com",
-  projectId: "tuo-progetto-id",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
+  apiKey: "AIzaSyBPLipRWjaEF593Png2nxqtyp8jerPWR-g",
+    authDomain: " rendez-vous-app-1c51d.firebaseapp.com",
+    projectId: "rendez-vous-app-1c51d",
+    storageBucket: "rendez-vous-app-1c51d.firebasestorage.app",
+    messagingSenderId: "29990031063",
+    appId: "1:29990031063:web:aa2952f0533a182d4daa92",
+    measurementId: "G-YPJ47QN9P7",
 };
 
 const app = initializeApp(firebaseConfig);
