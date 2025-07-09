e1ea35277ef070dbefe35375aac7056d6429eeec
Rendez-Vous-fastcar <coppamilanosanremo1906@gmail.com>
2025-07-09 13:02:37 +0200

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,11 +2,11 @@ importScripts('https://www.gstatic.com/firebasejs/10.5.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/10.5.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "TUA_API_KEY",
-  authDomain: "tuo-progetto.firebaseapp.com",
-  projectId: "tuo-progetto-id",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
+  apiKey: "TAIzaSyBPLipRWjaEF593Png2nxqtyp8jerPWR-g",
+  authDomain: "rendez-vous-app-1c51d.firebaseapp.com",
+  projectId: "rendez-vous-app-1c51d",
+  messagingSenderId: "29990031063",
+  appId: "1:29990031063:web:aa2952f0533a182d4daa92",
 });
 
 const messaging = firebase.messaging();
